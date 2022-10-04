9635f404b10dff2221545a6cefae059dbfdebab8
hermsale <hermsale@gmail.com>
2022-10-04 17:43:24 -0300

    se agrego la lista de peliculas en tendencia, y se empezo a trabajar en la lista de categorias

--- src/main.js
+++ src/main.js
@@ -4,18 +4,7 @@ const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
   });
 
-//   api.defaults.headers.common['X-API-KEY'] = API_KEY;
-
-//   movie/week 
-
-
-// solicitud a clonar 
-{/* <div class="trendingPreview-header">
-<h2 class="trendingPreview-title">Tendencias</h2>
-<button class="trendingPreview-btn">Ver más</button>
-</div> */}
-
-// solicitud de vista previa de peliculas en tendencia 
+  // solicitud de vista previa de peliculas en tendencia 
 async function getTrendingMoviesPreview(){
     const res = await api.get( '/trending/movie/week',{
         params:{
@@ -25,13 +14,55 @@ async function getTrendingMoviesPreview(){
     console.log(res.data.results);
 
     trendingPreviewMovieList = document.querySelector('.trendingPreview-movieList');
+    
     res.data.results.map(movie =>{
-        console.log(movie.title)
-        div = document.createElement('div');
-        div.classList = 'movie-container';
-    })
+        
+        divMovieContainer = document.createElement('div');
+        divMovieContainer.classList.add('movie-container');
 
+        imgMovie = document.createElement('img');
+        imgMovie.classList = 'movie-img';
+
+        imgMovie.src = 'https://image.tmdb.org/t/p/w300'+movie.poster_path;
+        imgMovie.alt = movie.title;
+
+        // div.appendChild(imgMovie);
+        divMovieContainer.appendChild(imgMovie);
+                trendingPreviewMovieList.appendChild(divMovieContainer);
+    })
+    
     
 }
 
 getTrendingMoviesPreview();
+
+async function getCategoriessPreview(){
+    const res = await api.get( '/genre/movie/list',{
+        params:{
+            api_key: API_KEY
+        }    
+    })
+    console.log(res.data.genres);
+
+    // trendingPreviewMovieList = document.querySelector('.trendingPreview-movieList');
+    
+    // res.data.results.map(movie =>{
+        
+    //     divMovieContainer = document.createElement('div');
+    //     divMovieContainer.classList.add('movie-container');
+
+    //     imgMovie = document.createElement('img');
+    //     imgMovie.classList = 'movie-img';
+
+    //     imgMovie.src = 'https://image.tmdb.org/t/p/w300'+movie.poster_path;
+    //     imgMovie.alt = movie.title;
+
+    //     // div.appendChild(imgMovie);
+    //     divMovieContainer.appendChild(imgMovie);
+    //             trendingPreviewMovieList.appendChild(divMovieContainer);
+    // })
+    
+    
+}
+
+getCategoriessPreview();
